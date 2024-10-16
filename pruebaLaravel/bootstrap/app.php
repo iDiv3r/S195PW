c900eb6894887bdb294c5eb2b8ddb838caa2e1c8
Div3r <udhgjkf@gmail.com>
2024-10-16 15:03:13 -0600

    laravel new practicaRepasos (Paso 1)

--- pruebaLaravel/bootstrap/app.php
+++ pruebaLaravel/bootstrap/app.php
@@ -6,9 +6,27 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        using:function(){
+
+            Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/emi.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/sebas.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/andy.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/robert.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/bris.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
--- pruebaLaravel/routes/web.php
+++ pruebaLaravel/routes/web.php
@@ -7,3 +7,12 @@ Route::get('/', function () {
 });
 
 Route::view('/asd','asd');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
